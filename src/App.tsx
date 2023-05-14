f5ec2a026458bb821f9eec57ace197a6260e256f
nazimboudeffa <boudeffa@gmail.com>
2023-05-14 04:23:27 +0200

    Added Navbar, Hero and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Navbar />
+    <Hero />
+    <Footer />
+    </>
   )
 }
